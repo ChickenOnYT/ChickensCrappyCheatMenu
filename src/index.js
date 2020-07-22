d0b11b6ebc88de917afd9a817faf92395e55c77c
DaChickenKing <bekfest25@gmail.com>
2020-07-22 10:40:26 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,25 +7,25 @@
   })();
 	
   // Closes wills cheat menu
-   const cheatmenuhide1 = document.querySelector('#cheat-menu');
-   cheatmenuhide1.style.display = 'none';
-   const cheatmenuhide2 = document.querySelector('#menu-toggle');
-   cheatmenuhide2 && (cheatmenuhide2.style.display = 'none');
+  const cheatmenuhide1 = document.querySelector('#cheat-menu');
+  cheatmenuhide1.style.display = 'none';
+  const cheatmenuhide2 = document.querySelector('#menu-toggle');
+  cheatmenuhide2 && (cheatmenuhide2.style.display = 'none');
 
-   // Insert menu
-   const menuWrapper = document.createElement("div");
-   menuWrapper.style.width = "100%";
-   menuWrapper.style.height = "25%";
-   menuWrapper.style.backgroundColor = "lightgrey";
-   menuWrapper.style.opacity = "0.95";
-   document.body.insertBefore(menuwrapper, document.body.firstChild);
+  // Insert menu
+  const menuWrapper = document.createElement("div");
+  menuWrapper.style.width = "100%";
+  menuWrapper.style.height = "25%";
+  menuWrapper.style.backgroundColor = "lightgrey";
+  menuWrapper.style.opacity = "0.95";
+  document.body.insertBefore(menuwrapper, document.body.firstChild);
 
-   function closeMenu() {
-     // Closes menu
-     menuWrapper.remove();
-     const cheatmenushow1 = document.querySelector('#cheat-menu');
-     cheatmenushow1.style.display = 'block';
-     const cheatmenushow2 = document.querySelector('#menu-toggle');
-     cheatmenushow2 && (cheatmenushow2.style.display = 'block');
-   }
+  function closeMenu() {
+    // Closes menu
+    menuWrapper.remove();
+    const cheatmenushow1 = document.querySelector('#cheat-menu');
+    cheatmenushow1.style.display = 'block';
+    const cheatmenushow2 = document.querySelector('#menu-toggle');
+    cheatmenushow2 && (cheatmenushow2.style.display = 'block');
+  }
 })();
