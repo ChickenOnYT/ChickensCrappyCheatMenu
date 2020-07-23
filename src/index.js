b2195f5d29e987eaa6fff93a7c4568368103de89
DaChickenKing <bekfest25@gmail.com>
2020-07-23 13:12:37 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,49 @@
 /* W.I.P. menu! */
-
 (() => {
-  // Import libraries
-  (async() => {
-     eval(await (await fetch("https://github.com/DaChickenKing/ChickensCrappyCheatMenu/blob/master/src/lib/swal2.js")).text())
-  })();
-	
-  // Closes wills cheat menu
-  const cheatmenuhide1 = document.querySelector('#cheat-menu');
-  cheatmenuhide1.style.display = 'none';
-  const cheatmenuhide2 = document.querySelector('#menu-toggle');
-  cheatmenuhide2 && (cheatmenuhide2.style.display = 'none');
+    // Import libraries
+    (async () => {
+        eval(await (await fetch("https://github.com/DaChickenKing/ChickensCrappyCheatMenu/blob/master/src/lib/swal2.js")).text());
+    })();
 
-  // Insert menu
-  const menuWrapper = document.createElement("div");
-  menuWrapper.style.width = "100%";
-  menuWrapper.style.height = "25%";
-  menuWrapper.style.backgroundColor = "lightgrey";
-  menuWrapper.style.opacity = "0.95";
-  document.body.insertBefore(menuwrapper, document.body.firstChild);
+    // Closes wills cheat menu
+    const cheatmenuhide1 = document.querySelector("#cheat-menu");
+    cheatmenuhide1.style.display = "none";
+    const cheatmenuhide2 = document.querySelector("#menu-toggle");
+    cheatmenuhide2 && (cheatmenuhide2.style.display = "none");
 
-  function closeMenu() {
-    // Closes menu
-    menuWrapper.remove();
-    const cheatmenushow1 = document.querySelector('#cheat-menu');
-    cheatmenushow1.style.display = 'block';
-    const cheatmenushow2 = document.querySelector('#menu-toggle');
-    cheatmenushow2 && (cheatmenushow2.style.display = 'block');
-  }
+    // Insert menu
+    const menuWrapper = document.createElement("div");
+    menuWrapper.style.width = "100%";
+    menuWrapper.style.height = "25%";
+    menuWrapper.style.backgroundColor = "lightgrey";
+    menuWrapper.style.opacity = "0.95";
+    document.body.insertBefore(menuwrapper, document.body.firstChild);
+
+    function closeMenu() {
+        // Closes menu
+        menuWrapper.remove();
+        const cheatmenushow1 = document.querySelector("#cheat-menu");
+        cheatmenushow1.style.display = "block";
+        const cheatmenushow2 = document.querySelector("#menu-toggle");
+        cheatmenushow2 && (cheatmenushow2.style.display = "block");
+    }
+
+    function inputDialog(title, btntext) {
+        Swal.fire({
+            title: title,
+            input: "text",
+            confirmButtonText: btntext,
+            inputValue: inputValue,
+            showCancelButton: true,
+            inputValidator: (value) => {
+                if (!value) {
+                    return "You need to write something!";
+                }
+            },
+        }).then((txt) => {
+            if (txt) {
+                return txt;
+            }
+        });
+    }
 })();
